8e1367cdfa03f5416dfcb271354df2aeacaded16
Caleb Sharp <epicsharp11@gmail.com>
2016-05-12 20:33:25 -0700

    Import default

--- test/computer_spec.js
+++ test/computer_spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-import { Computer } from '../src/computer.js';
+import Computer from '../src/computer.js';
 
 describe('Computer', () => {
   describe('constructor', () => {
