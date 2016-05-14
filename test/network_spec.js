fbcbdc564e921d19f6c5685243ba5a79465ef313
Caleb Sharp <epicsharp11@gmail.com>
2016-05-13 17:25:29 -0700

    Add network spec

---
+++ test/network_spec.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+
+import Network from '../src/network';
+
+describe('Network', () => {
+  describe('constructor', () => {
+    it('assigns the network name', () => {
+      const network = new Network('network1');
+      expect(network.name).to.equal('network1');
+    });
+
+    it('requires a the network name', () => {
+      expect(() => new Network()).to.throw(new TypeError("Missing required argument 'name'"));
+    });
+  });
+});
