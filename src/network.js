9915b0abd8495e0c2b92840ef4cce9b0c26db941
Caleb Sharp <epicsharp11@gmail.com>
2016-05-13 17:34:02 -0700

    Update Network class so test passes

--- src/network.js
+++ src/network.js
@@ -1,5 +1,11 @@
 class Network {
+  constructor(name) {
+    if (!name) {
+      throw new Error("Missing required argument 'name'");
+    }
 
+    this.name = name;
+  }
 }
 
 export default Network;
