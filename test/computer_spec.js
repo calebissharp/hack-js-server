592085a96e80bd659dfb3be3cc110b990bba5559
Caleb Sharp <epicsharp11@gmail.com>
2016-05-13 17:35:29 -0700

    Remove test case

--- test/computer_spec.js
+++ test/computer_spec.js
@@ -4,9 +4,5 @@ import Computer from '../src/computer.js';
 
 describe('Computer', () => {
   describe('constructor', () => {
-    it('should have a default ip', () => {
-      const computer = new Computer();
-      expect(computer.ip).to.equal('1.1.1.1');
-    });
   });
 });
