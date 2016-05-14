206812543249bec59ef7204b960ea4e1d1651ba6
Caleb Sharp <epicsharp11@gmail.com>
2016-05-13 17:39:38 -0700

    Test Network#connect

--- test/network_spec.js
+++ test/network_spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import Network from '../src/network';
+import Computer from '../src/computer';
 
 describe('Network', () => {
   describe('constructor', () => {
@@ -11,6 +12,22 @@ describe('Network', () => {
 
     it('requires the network name', () => {
       expect(() => new Network()).to.throw("Missing required argument 'name'");
+    });
+  });
+
+  describe('#connect', () => {
+    it('adds a computer to the network', () => {
+      const network = new Network();
+      network.connect(new Computer());
+
+      expect(network.computers[0]).to.be.an('Object');
+    });
+
+    it('assigns the provided ip to the computer', () => {
+      const network = new Network();
+      network.connect(new Computer(), '1.2.3.4');
+
+      expect(network.computers[0].ip).to.equal('1.2.3.4');
     });
   });
 });
